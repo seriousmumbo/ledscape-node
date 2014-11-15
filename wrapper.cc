26750830450e4e04d598adabdfe612eb81e79ba2
Ryan Leonard <rhinoisnot@gmail.com>
2014-11-15 13:01:03 -0500

    Added versions of functions without ledscape_wait

--- wrapper.cc
+++ wrapper.cc
@@ -73,6 +73,22 @@ static Handle<Value> LedscapeFillColor(const Arguments& args) {
   return v8::Boolean::New(true);
 }
 
+static Handle<Value> LedscapeFillColorNoWait(const Arguments& args) {
+  HandleScope scope;
+  const unsigned frame_num = args[0]->NumberValue();
+  const uint8_t r = args[1]->NumberValue();
+  const uint8_t g = args[2]->NumberValue();
+  const uint8_t b = args[3]->NumberValue();
+  printf("nodeledscape fillcolor %d %d %d ", r,g,b);
+  ledscape_frame_t * const frame = ledscape_frame(leds, frame_num);
+  for(unsigned i=0; i < num_pixels; i++) {
+    for(unsigned strip=0; strip < num_strips; strip++) {
+      ledscape_set_color(frame, strip, i, r, g, b);
+    }
+  }
+  return v8::Boolean::New(true);
+}
+
 static Handle<Value> LedscapeSetColor(const Arguments& args) {
   HandleScope scope;
   const unsigned frame_num = args[0]->NumberValue();
@@ -87,6 +103,19 @@ static Handle<Value> LedscapeSetColor(const Arguments& args) {
   return v8::Boolean::New(true);
 }
 
+static Handle<Value> LedscapeSetColorNoWait(const Arguments& args) {
+  HandleScope scope;
+  const unsigned frame_num = args[0]->NumberValue();
+  const unsigned strip_num = args[1]->NumberValue();
+  const unsigned pixel_num = args[2]->NumberValue();
+  const uint8_t r = args[3]->NumberValue();
+  const uint8_t g = args[4]->NumberValue();
+  const uint8_t b = args[5]->NumberValue();
+  ledscape_frame_t * const frame = ledscape_frame(leds, frame_num);
+  ledscape_set_color(frame, strip_num, pixel_num, r, g, b);
+  return v8::Boolean::New(true);
+}
+
 void InitAll(Handle<Object> exports, Handle<Object> module) {
    exports->Set(String::NewSymbol("init"),
      FunctionTemplate::New(LedscapeInit)->GetFunction());
@@ -94,8 +123,12 @@ void InitAll(Handle<Object> exports, Handle<Object> module) {
      FunctionTemplate::New(LedscapeDraw)->GetFunction()); 
    exports->Set(String::NewSymbol("fillColor"),
      FunctionTemplate::New(LedscapeFillColor)->GetFunction());
+   exports->Set(String::NewSymbol("fillColorNoWait"),
+     FunctionTemplate::New(LedscapeFillColorNoWait)->GetFunction());
    exports->Set(String::NewSymbol("setColor"),
      FunctionTemplate::New(LedscapeSetColor)->GetFunction());
+   exports->Set(String::NewSymbol("setColorNoWait"),
+     FunctionTemplate::New(LedscapeSetColorNoWait)->GetFunction());
    exports->Set(String::NewSymbol("close"),
      FunctionTemplate::New(LedscapeClose)->GetFunction());
 }
