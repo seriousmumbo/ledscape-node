0df55e45c8d7a5855ab71902c201bc3721248dd8
root <root@beaglebone.(none)>
2015-02-12 19:16:50 +0000

    fix fadesingle

--- build/anim/fadesingle.js
+++ build/anim/fadesingle.js
@@ -3,16 +3,19 @@ var color = require('color');
     
 ledscape.addData({
   ring: { control: { state: 'stopped' },
-          fade: { color: color('cyan'), period: 5.0 } },
+          fade: { color: color('cyan'), period: 15.0 } },
   test: { render: { color: color('blue'), pos: 5 } }
 })
 
 ledscape.addSystems({ 
   fade: function(ring) {
     elapsed = ledscape.data.frames.elapsed;
-    ratio = (1 - Math.cos(2*Math.PI*(elapsed * 1.0) / 
-             ring.fade.period)) / 2;
-    ledscape.fill(ring.fade.color.clone().darken(ratio));
+      ratio = (1 - Math.cos(2 * Math.PI * (elapsed * 1.0) / (ring.fade.period * 1.0))) / 2;
+    var clr = ring.fade.color.clone().rgb();
+    clr.r *= ratio;
+    clr.g *= ratio;
+    clr.b *= ratio;
+    ledscape.fill(clr);
   }
 });
 
